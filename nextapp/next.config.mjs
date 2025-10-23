420a97345324fcb1d0198011e98a0492a11fe1b0
Paul Schmidt <schmidtpsimon@outlook.de>
2025-10-24 00:18:31 +0200

    Add deploy workflow

--- nextapp/next.config.mjs
+++ nextapp/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/SitCheck',     // z. B. '/archive-frontend'
+  assetPrefix: '/SitCheck/', // muss identisch zum basePath sein
+};
 
 export default nextConfig;
