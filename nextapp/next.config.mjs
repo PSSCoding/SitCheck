05a76f993cc3d0f9b4b50c0510dcf26bb7087a2f
kiadmin <s232657@student.dhbw-mannheim.de>
2025-10-31 12:22:58 +0100

    VSC Setup

--- nextapp/next.config.mjs
+++ nextapp/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+//  output: 'export',
   images: { unoptimized: true },
   basePath: '/SitCheck',     // z. B. '/archive-frontend'
   assetPrefix: '/SitCheck/', // muss identisch zum basePath sein
